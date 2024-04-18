c5492ef991934ccecf5471b0a09d4a8e4d5d794c
imAshutoshGupta <aguptaworkspace@gmail.com>
2024-04-18 11:54:19 +0530

    functions remaining

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number')
+const operationButtons = document.querySelectorAll('[data-operation')
+const deleteButton = document.querySelector('[data-delete]')
+const equalsButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperand = document.querySelector('[data-previous]')
+const currentOperand = document.querySelector('[data-current]')
